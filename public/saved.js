a661b991012f78b602700a282c9c329af23f8c0d
Andrew Stiles <ndrwstls544@gmail.com>
2020-08-26 02:07:42 -0400

    added read more/less functionality

--- public/saved.js
+++ public/saved.js
@@ -22,8 +22,7 @@ function createBooks(bookArr) {
             contentDiv.appendChild(header)
             contentDiv.appendChild(helper.createAuthors(book.authors))
             if (book.description) {
-                let description = helper.createElement('div', ['description'], {'textContent': 'Description: ' + book.description})
-                contentDiv.appendChild(description)
+                shouldBookExpand(book, contentDiv)
             }
             let saveBtn = helper.createElement('button', ['red', 'ui', 'x', 'icon', 'button'], {'textContent': 'Remove Book ', 'id': book._id})
             let saveIcon = helper.createElement('i', ['x', 'icon'])
@@ -40,5 +39,37 @@ function createBooks(bookArr) {
         cardArea.appendChild(newMessage)
     }
 }
+// adding a read more or read less button if the character length of the description is over 250
+function shouldBookExpand(book, contentDiv) {
+    if (book.description.length > 250) {
+        let slicedDescription = book.description.slice(0, 251)
+        let descriptionDiv = helper.createElement('div', ['description-div']) 
+        let read = helper.createElement('span', ['read-more'], {'textContent': 'Read More...', 'data-expand': false })
+        read.style.color = 'blue'
+        read.style.marginLeft = '5px'
+        read.style.cursor = 'pointer'
+        let description = helper.createElement('span', ['description'], {'textContent': 'Description ' + slicedDescription})
+        read.addEventListener('click', function(e) {
+            if (e.target['data-expand']) {
+                e.target['data-expand'] = false
+                description.textContent = 'Description: ' + slicedDescription
+                e.target.textContent = 'Read More...'
+            }
+            else {
+                e.target['data-expand'] = true
+                description.textContent = 'Description: ' + book.description
+                e.target.textContent = 'Read Less...'
+            }
+        })
+        descriptionDiv.appendChild(description)
+        descriptionDiv.appendChild(read)
+        contentDiv.appendChild(descriptionDiv)
+    }
+    else {
+        let description = helper.createElement('div', ['description'], {'textContent': 'Description: ' + book.description}) 
+        contentDiv.appendChild(description)
+    }
+}
+
 
 getBooks()
